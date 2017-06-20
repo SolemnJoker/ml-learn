6c7bd0555f4f8482c84695808833a55ad2000c73
SolemnJoker <wenxiongkjsm@163.com>
2017-06-20 18:43:26 +0800

    update : createDataset splitDataset chooseBestFeat2Split

--- decision_tree/tree.py
+++ decision_tree/tree.py
@@ -1,14 +1,59 @@
 #-*- coding: UTF-8 -*-
 from math import log
 
+#计算香农熵
 def calShannonEnt(dataset):
-    data_size = len(dataset)
-    label_count = {}
-    for feat_vec in dataset:
-        cur_label = feat_vec[-1]
-        label_count[cur_label] = label_count.get(cur_label,0) + 1
-        shannon_ent = 0.0
-    for key in label_count:
-        prob = label_count[key]/data_size
-        shannon_ent -= prob* log(prob,2)
-    return shannon_ent
+    dataSize = len(dataset)
+    labelCount = {}
+    for featVec in dataset:
+        curLabel = featVec[-1]
+        labelCount[curLabel] = labelCount.get(curLabel,0) + 1
+        shannonEnt = 0.0
+    for key in labelCount:
+        prob = float(labelCount[key])/dataSize
+        shannonEnt -= prob* log(prob,2)
+    return shannonEnt
+
+def createDataset():
+    dataset = [[1,1,'yes'],
+               [1,1,'yes'],
+               [1,0,'no'],
+               [0,1,'no'],
+               [1,0,'no'],
+               [1,1,'yes']]
+    labels = ['no surfacing','flippers']
+    return dataset,labels
+
+#划分子数据集
+def splitDataset(dataset,axis,value):
+    retDataset = []
+    for featVec in dataset:
+        if featVec[axis] == value:
+            retFeatVec = featVec[:axis]
+            retFeatVec.extend(featVec[axis+1:])
+            retDataset.append(retFeatVec)
+    return retDataset
+
+def chooseBestFeat2Split(dataset):
+    bestFeat = -1
+    bestInfoGain = 0.0
+    featNum = len(dataset[0]) - 1
+    baseEntropy = calShannonEnt(dataset)
+    for i in range(featNum):
+        featList = [X[i] for X in dataset]
+        uniqueFeatValue = set(featList)
+        newEntropy = 0.0
+        for value in uniqueFeatValue:
+            subDataset = splitDataset(dataset,i,value)
+            prob = float(len(subDataset))/len(dataset)
+            newEntropy += prob*calShannonEnt(subDataset)
+        infoGain = baseEntropy - newEntropy
+        if infoGain > bestInfoGain:
+            bestInfoGain = infoGain
+            bestFeat = i
+    return bestFeat
+
+#test
+dataset,labels = createDataset()
+print chooseBestFeat2Split(dataset)
+
