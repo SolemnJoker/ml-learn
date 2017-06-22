709348f26da01b54ca9994d6b31db78d393f5b97
SolemnJoker <wenxiongkjsm@163.com>
2017-06-22 18:40:44 +0800

    update draw tree

--- decision_tree/tree.py
+++ decision_tree/tree.py
@@ -2,72 +2,84 @@
 from math import log
 import operator
 
-#计算香农熵
+# 计算香农熵
+
+
 def calShannonEnt(dataset):
     dataSize = len(dataset)
     labelCount = {}
     for featVec in dataset:
         curLabel = featVec[-1]
-        labelCount[curLabel] = labelCount.get(curLabel,0) + 1
+        labelCount[curLabel] = labelCount.get(curLabel, 0) + 1
         shannonEnt = 0.0
     for key in labelCount:
-        prob = float(labelCount[key])/dataSize
-        shannonEnt -= prob* log(prob,2)
+        prob = float(labelCount[key]) / dataSize
+        shannonEnt -= prob * log(prob, 2)
     return shannonEnt
 
-def createDataset():
-    dataset = [[1,1,'yes'],
-               [1,1,'yes'],
-               [1,0,'no'],
-               [0,1,'no'],
-               [1,0,'no'],
-               [1,1,'yes']]
-    labels = ['no surfacing','flippers']
-    return dataset,labels
 
-#划分子数据集
-def splitDataset(dataset,axis,value):
+def createDataset():
+    dataset = [[0, 1, 1, 'yes'],
+               [1, 1, 1, 'yes'],
+               [1, 0, 1, 'yes'],
+               [1, 1, 0, 'no'],
+               [0, 0, 1, 'no'],
+               [1, 1, 0, 'no'],
+               [0, 0, 0, 'yes']]
+    labels = ['no surfacing', 'flippers', 'head']
+    return dataset, labels
+
+# 划分子数据集
+
+
+def splitDataset(dataset, axis, value):
     retDataset = []
     for featVec in dataset:
         if featVec[axis] == value:
             retFeatVec = featVec[:axis]
-            retFeatVec.extend(featVec[axis+1:])
+            retFeatVec.extend(featVec[axis + 1:])
             retDataset.append(retFeatVec)
     return retDataset
 
-#选择最佳划分
+# 选择最佳划分
+
+
 def chooseBestFeat2Split(dataset):
     bestFeat = -1
     bestInfoGain = 0.0
     featNum = len(dataset[0]) - 1
+    print 'featNum:',featNum
     baseEntropy = calShannonEnt(dataset)
     for i in range(featNum):
         featList = [X[i] for X in dataset]
         uniqueFeatValue = set(featList)
         newEntropy = 0.0
         for value in uniqueFeatValue:
-            subDataset = splitDataset(dataset,i,value)
-            prob = float(len(subDataset))/len(dataset)
-            newEntropy += prob*calShannonEnt(subDataset)
+            subDataset = splitDataset(dataset, i, value)
+            prob = float(len(subDataset)) / len(dataset)
+            newEntropy += prob * calShannonEnt(subDataset)
         infoGain = baseEntropy - newEntropy
         if infoGain > bestInfoGain:
             bestInfoGain = infoGain
             bestFeat = i
+
     return bestFeat
 
-#选择classlist中数量最多的类别
+# 选择classlist中数量最多的类别
+
+
 def majorityCnt(classlist):
     classCount = {}
     for c in classlist:
-        classCount[c] = classCount.get(c,0) + 1
-    
-    sortClassCount = sorted(classCount.iteritems(),
-                     key = operator.itemgetter(1),reverse=True)
-    return sortClassCount[0][0]
-        
+        classCount[c] = classCount.get(c, 0) + 1
 
-#构建决策树
-def createTree(dataset,labels):
+    sortClassCount = sorted(classCount.iteritems(),
+                            key=operator.itemgetter(1), reverse=True)
+    return sortClassCount[0][0]
+
+
+# 构建决策树
+def createTree(dataset, labels):
     curLabels = labels[:]
     classlist = [c[-1] for c in dataset]
     if classlist.count(classlist[0]) == len(classlist):
@@ -76,12 +88,17 @@ def createTree(dataset,labels):
         return majorityCnt(classlist)
     bestFeat = chooseBestFeat2Split(dataset)
     bestLabel = curLabels[bestFeat]
-    curTreeNode = {bestLabel:{}}
-    del(labels[bestFeat])
+    curTreeNode = {bestLabel: {}}
+    del(curLabels[bestFeat])
     featlist = [f[bestFeat] for f in dataset]
     uniqueFeatValue = set(featlist)
     for value in uniqueFeatValue:
         subLabels = curLabels[:]
         curTreeNode[bestLabel][value] = createTree(
-            splitDataset(dataset,bestFeat,value),subLabels)
+            splitDataset(dataset, bestFeat, value), subLabels)
     return curTreeNode
+
+
+def createAtree():
+    dataset, labels = createDataset()
+    return createTree(dataset, labels)
--- decision_tree/treePlotter.py
+++ decision_tree/treePlotter.py
@@ -1,43 +1,102 @@
 import matplotlib.pyplot as plt
+import tree as t
 
 def getNumOfLeafs(curTreeNode):
-    numLeafs = 0
-    childNodes = curTreeNode.values()
+    numLeafs = 1
+    childNodes = curTreeNode.values()[0]
     for key in childNodes.keys():
         if type(childNodes[key]) == type({}):
-            numLeafs += getNumOfLeafs(childNodes[key])
+           numLeafs += getNumOfLeafs(childNodes[key])
         else:
             numLeafs += 1
     return numLeafs
 
+
 def getTreeDepth(curTreeNode):
     maxDepth = 0
-    childNodes = curTreeNode.values()
+    childNodes = curTreeNode.values()[0]
     curDepth = 1
     for key in childNodes.keys():
         if type(childNodes[key]) == type({}):
             curDepth += getTreeDepth(childNodes[key])
-    if curDepth > maxDepth: maxDepth = curDepth
+    if curDepth > maxDepth:
+        maxDepth = curDepth
     return maxDepth
 
-def plotMidText(childPos,parentPos,textStr):
-    midX = (childPos[0] + parentPos[0])/2.0
-    midY = (childPos[1] + parentPos[1])/2.0
-    createPlot.ax1.text(midX,midY,textStr,va='center',ha='center',rotation=90)
+decisionNode = dict(boxstyle='sawtooth',fc='0.8')
+leafNode = dict(boxstyle='round4',fc='0.8')
+arrow_args = dict(arrowstyle="<-")
+def plotNode(nodeText,childPt,parentPt,nodeType):
+    createPlot.ax1.annotate(nodeText,xy=parentPt,xycoords='axes fraction',xytext=childPt,
+    textcoords='axes fraction',va='center',ha='center',bbox=nodeType,arrowprops=arrow_args)
 
-def plotTree(curTreeNode,parentPos,nodeText):
+def plotMidText(childPos, parentPos, textStr):
+    midX = (childPos[0] + parentPos[0]) / 2.0
+    midY = (childPos[1] + parentPos[1]) / 2.0
+    createPlot.ax1.text(midX, midY, textStr, va='center',ha='center')
+
+
+def plotTree(curTreeNode, leftTopPos,parentPos, nodeText):
     numLeafs = getNumOfLeafs(curTreeNode)
     depth = getTreeDepth(curTreeNode)
-    firstStr=curTreeNode.keys()[0]
+    firstStr = curTreeNode.keys()[0]
+    childs = curTreeNode[firstStr]
+    numList=[]
+    curIndex = 0
+    i = 0
+    childslen = len(childs)
+    for key in childs.keys():
+        if type(childs[key]) == type({}):
+            numList.append(getNumOfLeafs(childs[key]))
+            if i < childslen/2 :
+                curIndex +=numList[-1] 
+        else:
+            numList.append(1)
+            if i < childslen/2 :
+                curIndex += 1
+        i+=1
+
+    if childslen % 2 ==0:
+        pass
+    else:
+        curIndex = sum(numList)/2 + 1
     
+    
+ 
+    curPos = (leftTopPos[0]  + curIndex/plotTree.totalW,leftTopPos[1])
+    firstChildPos = (leftTopPos[0] ,leftTopPos[1] - 1/plotTree.totalD)
+    curChildX,curChildY = firstChildPos
+    if leftTopPos == parentPos:
+        parentPos = curPos
+    plotNode(firstStr,curPos,parentPos,decisionNode)
+    plotMidText(curPos,parentPos,nodeText)
+    i = 0
+    for key in childs.keys():
+        if i == len(childs)/2:
+            curChildX += 1.0/plotTree.totalW
+        i += 1
+        if type(childs[key]) == type({}):
+            childNumLeafs,childDepth = plotTree(childs[key],(curChildX,curChildY),curPos,str(key))
+            curChildX += childNumLeafs/plotTree.totalW
+        else:
+            plotNode(childs[key],(curChildX,curChildY),curPos,leafNode)
+            plotMidText((curChildX,curChildY),curPos,str(key))
+            curChildX += 1.0/plotTree.totalW
+    
+    return numLeafs,depth
+
+
+
 def createPlot(inTree):
-    fig = plt.figure(1,facecolor='0xffffff')
+    fig = plt.figure(1, facecolor='#ffffff')
     fig.clf()
-    axprops = dict(xticks=[],yticks=[])
-    createPlot.ax1 = plt.subplot(111,frameon=False,**axprops)
+    axprops = dict(xticks=[], yticks=[])
+    createPlot.ax1 = plt.subplot(111, frameon=False, **axprops)
     plotTree.totalW = float(getNumOfLeafs(inTree))
     plotTree.totalD = float(getTreeDepth(inTree))
-    plotTree.xOff = -0.5/plottree.totalW
-    plottree.yOff = 1.0 
-    plottree(inTree,(0.5,1.0),'')
+    plotTree(inTree, (0.0, 1.0),(0.0,1.0), '')
     plt.show()
+
+tree = t.createAtree();
+print tree
+createPlot(tree)
