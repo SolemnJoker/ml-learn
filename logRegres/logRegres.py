3a3fb0779f1b2e17924d3a88cc9a99677a1f914b
SolemnJoker <wenxiongkjsm@163.com>
2017-06-30 14:14:43 +0800

    update

--- logRegres/logRegres.py
+++ logRegres/logRegres.py
@@ -1,8 +1,9 @@
 from numpy import *
 import os
+import matplotlib.pylab as plt
 
 def loadData():
-    f = open('testSet.txt')
+    f = open(os.path.dirname(__file__) + '/testSet.txt')
     lines = f.readlines()
     dataSet = []
     labels  = []
@@ -27,4 +28,24 @@ def gradAscent(dataSet,labels):
         error = labelMat - h
         weights = weights + alpha*dataMat.transpose()*error
     return weights
+
+def plotBestFit(dataset,labels,weights):
+    xcord = [[],[]];ycord=[[],[]]
+    n = shape(dataset)[0]
+    for i in range(n):
+        xcord[int(labels[i])].append(dataset[i][0])
+        ycord[int(labels[i])].append(dataset[i][1])
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.scatter(xcord[0],ycord[0],s = 30,c='red',marker='s')
+    ax.scatter(xcord[1],ycord[1],s = 30,c='blue')
+    x = arange(-3.0,3.0,0.1)
+    y = -(weights.item(0)*x + weights.item(2)) /weights.item(1)
+    ax.plot(x,y)
+    plt.show()
+
+dataSet,labels = loadData()
+weights = gradAscent(dataSet,labels)
+plotBestFit(dataSet,labels,weights)
+
         
