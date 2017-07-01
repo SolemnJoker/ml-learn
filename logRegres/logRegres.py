bbd8d831db20f1ac441a3a568f700846e03fded1
Solemnjoker <wenxiongkjsm@163.com>
2017-07-01 23:11:48 +0800

    update

--- logRegres/logRegres.py
+++ logRegres/logRegres.py
@@ -1,8 +1,10 @@
+#-*- coding: UTF-8 -*-
 from numpy import *
+import matplotlib.pyplot as plt
 import os
 
 def loadData():
-    f = open('testSet.txt')
+    f = open(os.path.dirname(__file__)+'/testSet.txt')
     lines = f.readlines()
     dataSet = []
     labels  = []
@@ -29,24 +31,37 @@ def gradAscent(dataSet,labels):
     return weights
 
 def stocGradAscent0(dataSet,labels):
+    dataSet = array(dataSet)
     s=shape(dataSet)
     weights =ones(s[1])
+    alpha = 0.01
     for i in range(s[0]):
-        h = simgoid(dataMat[i]*weights)
-        error = labelMat[i] - h
-        weights = weights + alpha*dataMat[i]*error
+        h = simgoid((dataSet[i]*weights))
+        error = labels[i] - h
+        print i,s,dataSet[i]*error,error
+        weights = weights + alpha*dataSet[i]*error
     return weights
  
 def stocGradAscent1(dataSet,labels,numInter = 150):
+    dataSet = array(dataSet)
     s=shape(dataSet)
     weights =ones(s[1])
-    for j in range(numInter):
+    for j in range(numInter):      
         dataIndex = range(s[0]) 
         for i in range(s[0]):
-            randIndex = int(random.uniform(0,s[0]))
-            h = simgoid(dataMat[randIndex]*weights)
-            error = labelMat[randIndex] - h
-            weights = weights + alpha*dataMat[randIndex]*error
+            randIndex = int(random.uniform(0,len(dataIndex)))
+            h = simgoid(sum(dataSet[randIndex]*weights))
+            alpha = 4/(1.0+i+j) + 0.01
+            error = labels[randIndex] - h
+            weights = weights + alpha*dataSet[randIndex]*error
+            print len(dataIndex),randIndex
             del(dataIndex[randIndex])
     return weights
- 
+
+
+
+dataset,labels = loadData()
+weights = gradAscent(dataset,labels)
+
+weights = stocGradAscent0(dataset,labels)
+
