d8da60463142c27cd2d47ea1cba5f410f0153b66
SolemnJoker <wenxiongkjsm@163.com>
2017-08-21 17:40:10 +0800

    08 gan update

--- tensorflow-learn/08_gan_mnist.py
+++ tensorflow-learn/08_gan_mnist.py
@@ -1,6 +1,9 @@
 import tensorflow as tf
 import tensorflow.examples.tutorials.mnist.input_data as input_data
-import numpy
+import numpy as np
+import sys
+import time
+import pickle
 
 def get_inputs(real_img_size,noise_size):
     real_img = tf.placeholder(tf.float32,[None,real_img_size])
@@ -50,7 +53,7 @@ g_units = 128
 d_units = 128
 #leaky ReLU参数
 alpha=0.01
-learning_rate=0.001
+learning_rate=0.005
 #label smoothing??
 smooth=0.1
 
@@ -63,6 +66,127 @@ g_logits,g_outputs = get_generator(noise_data,g_units,img_size)
 
 #discriminator
 d_logits_real,d_outputs_real = get_discriminator(real_img,d_units)
-d_logits_fake,d_outputs_fake = get_discriminator(g_outputs,d_units,reuse=True)
+d_logits_fake,d_outputs_fake = get_discriminator(g_outputs,d_units,
+reuse=True)
+
+#Loss 1-smooth??
+#真实图片loss
+# tf.ones_like生成和tensor shape一样的全为1的tensor，zero_like同理
+# 'tensor' is [[1, 2, 3], [4, 5, 6]] 
+#  tf.ones_like(tensor) ==> [[1, 1, 1], [1, 1, 1]]
+d_loss_real = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits (
+     logits=d_logits_real,labels=tf.ones_like(d_logits_real))*(1-smooth))
+
+#生成图片loss
+d_loss_fake =tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits (
+    logits=d_logits_fake,labels=tf.zeros_like(d_logits_fake))*(1-smooth))
+
+#总的discriminator loss
+d_loss = d_loss_fake  + d_loss_real
+
+#generator loss
+g_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=
+d_logits_fake,labels=tf.ones_like(d_logits_fake)) *(1-smooth))
+
+
+#Optimizer
+#tf.trainable_variables返回的是需要训练的变量列表
+train_vars = tf.trainable_variables()
+g_var = [var for var in train_vars if var.name.startswith("generator")]
+d_var = [var for var in train_vars if var.name.startswith("discriminator")]
+
+#optimizer
+d_train_op = tf.train.AdamOptimizer(learning_rate).minimize(d_loss,var_list=d_var)
+g_train_op = tf.train.AdamOptimizer(learning_rate).minimize(g_loss,var_list=g_var)
+
+#训练
+batch_size = 50
+epochs = 300
+n_sample = 25
+samples = []
+losses = []
+saver = tf.train.Saver(var_list=g_var)
+
+with tf.Session() as sess:
+    with tf.device("/cpu:0"):
+        start_time = time.clock()
+        sess.run(tf.global_variables_initializer())
+        for e in range(epochs):
+            epoch_start = time.clock()
+            for batch_i in range(int(mnist.train.num_examples/batch_size)):
+                batch = mnist.train.next_batch(batch_size)
+                batch_images = batch[0].reshape((batch_size,784))
+                #tanh输出（-1，1）转换到（0，1）
+                batch_images = batch_images*2 - 1
+                batch_noise = np.random.uniform(-1,1,size=(batch_size,noise_size))
+                sess.run(d_train_op,feed_dict={real_img:batch_images,noise_data:batch_noise})
+                sess.run(g_train_op,feed_dict={noise_data:batch_noise})
+
+            train_loss_d = sess.run(d_loss,feed_dict={real_img:batch_images,
+                noise_data:batch_noise})
+            train_loss_d_real = sess.run(d_logits_real,feed_dict={
+                real_img:batch_images,noise_data:batch_noise})
+            train_loss_d_fake = sess.run(d_logits_fake,feed_dict={
+                real_img:batch_images,noise_data:batch_noise})
+
+            train_loss_g = sess.run(g_loss,feed_dict={noise_data:batch_noise})
+
+
+            print("Epoch {}/{}...".format(e+1, epochs),
+                  "Discriminator Loss: {:.4f}...".format(train_loss_d),
+                  "Generator Loss: {:.4f}".format(train_loss_g))    
+            sys.stdout.flush()
+            # 记录各类loss值
+            losses.append((train_loss_d, train_loss_d_real, train_loss_d_fake, train_loss_g))
+
+            # 抽取样本后期进行观察
+            sample_noise = np.random.uniform(-1, 1, size=(n_sample, noise_size))
+            gen_samples = sess.run(get_generator(noise_data, g_units, img_size, reuse=True),
+                                   feed_dict={noise_data: sample_noise})
+            samples.append(gen_samples)
+            epoch_end = time.clock()
+            print("epoch time:{}".format(epoch_end - epoch_start))
+            sys.stdout.flush()
+
+            # 存储checkpoints
+            saver.save(sess, './checkpoints/generator.ckpt')
+        end_time = time.clock()
+        sys.stdout.flush()
+        print("total time:{}".format(end_time - start_time))
+
+# 将sample的生成数据记录下来
+with open('train_samples.pkl', 'wb') as f:
+    pickle.dump(samples, f)
+ 
+fig, ax = plt.subplots(figsize=(20,7))
+losses = np.array(losses)
+plt.plot(losses.T[0], label='Discriminator Total Loss')
+plt.plot(losses.T[1], label='Discriminator Real Loss')
+plt.plot(losses.T[2], label='Discriminator Fake Loss')
+plt.plot(losses.T[3], label='Generator')
+plt.title("Training Losses")
+plt.legend()
+        
+with open('train_samples.pkl', 'rb') as f:
+    samples = pickle.load(f)
+
+def view_samples(epoch, samples):
+    """
+    epoch代表第几次迭代的图像
+    samples为我们的采样结果
+    """
+    fig, axes = plt.subplots(figsize=(7,7), nrows=5, ncols=5, sharey=True, sharex=True)
+    for ax, img in zip(axes.flatten(), samples[epoch][1]): # 这里samples[epoch][1]代表生成的图像结果，而[0]代表对应的logits
+        ax.xaxis.set_visible(False)
+        ax.yaxis.set_visible(False)
+        im = ax.imshow(img.reshape((28,28)), cmap='Greys_r')
+    
+    return fig, axes
+
+_ = view_samples(-1, samples) # 显示最后一轮的outputs
+
+
+
+
 
 
